0123d5b6430d00ca472197463395df3f119ce14b
“FiNeil <202197680wtudent.gla.ac.uk>
2017-01-24 16:29:18 +0000

    Add about html note does not work

--- rango/views.py
+++ rango/views.py
@@ -5,9 +5,13 @@ from django.http import HttpResponse
 def index(request):
       # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    context_dict = {'boldmessage': "Boo! I'm a hedgehog! "}
     
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/index.html', context=context_dict)
+	
+
+def about(request):	
+	return render(request, 'rango/about.html', context=context_dict) #I did this it worng!
